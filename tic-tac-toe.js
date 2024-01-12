6419295d1b5bb54673890e667c9ef48d6d00ebf6
Anannya Preeta <2020977@iub.edu.bd>
2024-01-12 23:39:58 +0600

    latest commit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,50 @@
+function audio(){
+    const click = new Audio("mouse-click-effect.wav"); 
+    click.play();
+}
+let playerInput = document.getElementById('status')
+let restartbtn = document.getElementById('restart')
+let boxes = Array.from(document.getElementsByClassName('box'))
 
+const o_letter = "O"
+const x_letter = "X"
+let firstLetter = x_letter
+let spaces = Array(9).fill(null)
+
+const startGame = () =>{
+    boxes.forEach(box => box.addEventListener('click',boxClick))
+
+}
+function boxClick(hehe){
+    const id = hehe.target.id
+    if(!spaces[id]){
+        spaces[id] = firstLetter
+        hehe.target.innerText = firstLetter
+        if(win()){
+            playerInput = `${firstLetter} has won!`
+        }
+        firstLetter = firstLetter == x_letter ? o_letter :  x_letter
+    }
+}
+
+const winCombo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+function win(){
+    for(const condition of winCombo){
+        let [a,b,c] = condition
+
+        if(spaces[a] && spaces[a]==spaces[b] && spaces[a]==spaces[c]){
+            return [a,b,c]
+        }
+        return false
+    }
+}
+restartbtn.addEventListener('click', restart)
+function restart(){
+    spaces.fill(null)
+    boxes.forEach(box=>{
+        box.innerText = ''
+    })
+    playerInput = 'Tic Tac Toe'
+    firstLetter = x_letter
+}
+startGame()
